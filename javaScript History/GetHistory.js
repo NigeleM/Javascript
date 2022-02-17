bffa161cf08bf13c9ffc7e1fd49f4e688cc1c9ef
Nigele <86542859+NigeleM@users.noreply.github.com>
2022-02-16 21:34:49 -0500

    Javascript browser recon
    java script browser recon, still researching

---
+++ javaScript History/GetHistory.js
@@ -0,0 +1,13 @@
+
+
+function Page() {
+    document.write("<p>Browser : " + window.navigator.vendor + "</p>")
+    document.write("<p>Browser : " + window.navigator.userAgent+ "</p>")
+    
+}
+
+
+function getHistory(){
+
+        Page()
+}
